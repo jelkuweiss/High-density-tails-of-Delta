45b58b3ce41d4a6991bb3b73a1517fa1a411ba94
Jamal El Kuweiss <61908177+jelkuweiss@users.noreply.github.com>
2023-02-24 16:06:19 +0100

    Added Characteristic Function tools

--- functions.py
+++ functions.py
@@ -245,3 +245,122 @@ def RCGF_at_the_saddle(kl, pl, knl, pnl, R, delta_min=-1.5, delta_max=1.4):
     Psi_final = np.array([Psi, Psi_1, Psi_2, Psi_3, Psi_4, Psi_5])
 
     return delta_L, Lam_final, cgf_final, cgfp_final, cgfpp_final, Psi_final
+
+def RCGF_at_the_saddle2(kl, pl, knl, pnl, R, delta_min=-10, delta_max=1.5):
+    # Expression of the CGF equated at the saddle point of the action (equation [5] of https://arxiv.org/abs/1912.06621)
+    Lam = []  # lambda values
+    cgf = []  # CGF values
+
+    # The actual delta values that we return
+    delta_L, delta_step = np.linspace(delta_min, delta_max, num=2 ** 13, retstep=True)
+
+    for delta in delta_L:
+        Rl = np.power(1 + approximate_collapse_alex(delta), 1 / 3) * R
+        # collapses
+        F = approximate_collapse_alex(delta)
+        Fp = derivative_of_approximate_collapse_alex(delta)
+        # sigmas
+        sRl = sigma_from_power_spectrum(kl, pl, Rl)
+        # sigma squares
+        sRl2 = np.square(sRl)
+
+        # jstar as a function of detla
+        j = delta / sRl2
+
+        # value of the derivative of the CGF wrt R'
+        delCGF = 0.5 * (np.square(j)) * del_variance(kl, pl, Rl)
+        # value of the lambda
+        R_factor = np.power(R, 3) / (2 * np.power(Rl, 2))
+        lam = +(j / Fp) - (delCGF * R_factor)
+
+        Lam += [lam]
+        cgf += [+ (lam * F) - (j * delta) + (0.5 * np.square(j) * sRl2)]
+    Lam = np.array(Lam)
+    cgf = np.array(cgf)
+
+    var_ratio = np.square(sigma_from_power_spectrum(kl, pl, R)) / np.square(
+        sigma_from_power_spectrum(knl, pnl, R))
+    # Rescaling both the cgf values and the lambda values to get the RCGF
+    cgf = cgf * var_ratio
+    Lam = Lam * var_ratio
+
+    cgf_p = np.diff(cgf) / np.diff(Lam)
+    lam_p = 0.5 * (Lam[1:] + Lam[:-1])
+    cgf_pp = np.diff(cgf_p) / np.diff(lam_p)
+    lam_pp = 0.5 * (lam_p[1:] + lam_p[:-1])
+
+    # delta values for which we have all derivatives
+    Lam_new = lam_pp
+    cgf = np.interp(Lam_new, Lam, cgf)
+    cgf_p = np.interp(Lam_new, lam_p, cgf_p)
+    # cgf_pp = np.interp(Lam_new, lam_pp, cgf_pp)
+
+    # Now we evaluate the values of the Psi function
+    rho_new = cgf_p
+    Psi = Lam_new * rho_new - cgf
+    Psi_1 = Lam_new
+    Psi_2 = np.diff(Psi_1) / np.diff(rho_new)
+    rho_2 = 0.5 * (rho_new[1:] + rho_new[:-1])
+    Psi_3 = np.diff(Psi_2) / np.diff(rho_2)
+    rho_3 = 0.5 * (rho_2[1:] + rho_2[:-1])
+    Psi_4 = np.diff(Psi_3) / np.diff(rho_3)
+    rho_4 = 0.5 * (rho_3[1:] + rho_3[:-1])
+    Psi_5 = np.diff(Psi_4) / np.diff(rho_4)
+    rho_5 = 0.5 * (rho_4[1:] + rho_4[:-1])
+
+    # and now to reduce the psi's too
+    rho_final = rho_5
+    Psi = np.interp(rho_final, rho_new, Psi)
+    Psi_1 = np.interp(rho_final, rho_new, Psi_1)
+    Psi_2 = np.interp(rho_final, rho_2, Psi_2)
+    Psi_3 = np.interp(rho_final, rho_3, Psi_3)
+    Psi_4 = np.interp(rho_final, rho_4, Psi_4)
+    # Psi_5 = np.interp(rho_final, rho_5, Psi_5)
+    Psi_final = np.array([rho_final, Psi, Psi_1, Psi_2, Psi_3, Psi_4, Psi_5])
+
+    return delta_L, Lam_new, cgf, cgf_p, cgf_pp, Psi_final
+
+
+def characteristic_function_from_definition(del_R, kl, pl, knl, pnl, R, order=10):
+    Psi_0 = []
+    var_ratio = np.square(sigma_from_power_spectrum(kl, pl, R)) / np.square(
+        sigma_from_power_spectrum(knl, pnl, R))
+    for d in del_R:
+        RL = np.power(1 + d, 1 / 3) * R
+        Psi_0 += [(1 / (2 * np.square(sigma_from_power_spectrum(kl, pl, RL)))) * np.square(
+            inverse_of_approximate_collapse_alex(d)) * var_ratio]
+
+    # Getting the final array of rhos where are derivatives would be defined
+    rho_final = del_R
+    for i in range(order):
+        rho_final = 0.5 * (rho_final[1:] + rho_final[:-1])
+
+    # Getting the derivates and the rho's at which they are defined and then interpolating on the rho_final
+    Psi = np.array([np.interp(rho_final, del_R, Psi_0)])
+    psi_deriv = Psi_0
+    rho = del_R
+    for i in range(order):
+        psi_deriv = np.diff(psi_deriv) / np.diff(rho)
+        rho = 0.5 * (rho[1:] + rho[:-1])
+        Psi = np.append(Psi, np.array([np.interp(rho_final, rho, psi_deriv)]), axis=0)
+
+    # Getting the critical
+    return rho_final, Psi
+
+
+## General Utility Functions
+
+def accurate_first_order_derivative(f, x):
+    # assuming uniform grid, otherwise provided an x, the f will be interpolated onto a uniform grid
+    uniform_x, step = np.linspace(np.nanmin(x), np.nanmax(x), num=2 * len(x), retstep=True)
+    f = np.interp(uniform_x, x, f)
+    x = uniform_x
+
+    fp = []
+    xp = []
+    for i in range(len(f)):
+        if (i > 3) & (i < len(f) - 4):
+            fp += [(((1 / 280) * (f[i - 4] - f[i + 4])) + ((4 / 105) * (f[i + 3] - f[i - 3])) + (
+                        (1 / 5) * (f[i - 2] - f[i + 2])) + ((4 / 5) * (f[i + 1] - f[i - 1]))) / step]
+            xp += [x[i]]
+    return np.array(fp), np.array(xp)
